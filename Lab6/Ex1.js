e6bb861aa31a299233b6d802da8bfc743d4987af
jlau19 <hl22@hawaii.edu>
2020-09-22 13:07:14 -1000

    xx

---
+++ Lab6/Ex1.js
@@ -0,0 +1,7 @@
+var age =  "23";
+var fav_num = 19;
+var day_of_birth = 19;
+var month_of_birth = 12;
+
+console.log(fav_num / day_of_birth * month_of_birth);
+console.log((age + fav_num) / day_of_birth * month_of_birth);
---
+++ Lab6/Ex3.js
@@ -0,0 +1,5 @@
+n = 23;
+m = 2;
+result = n * m++;
+
+console.log(result,n,m);
