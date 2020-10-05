06cc74ef04c7f94c9074efdd1d0173d16cca6754
jlau19 <hl22@hawaii.edu>
2020-10-04 22:36:37 -1000

    xx

---
+++ Lab8/EC.js
@@ -0,0 +1,7 @@
+age_count = 1; // start the age count
+age = 23;
+while(age_count < Math.sqrt(age)) {
+    console.log(`age ${age_count}`);
+    age_count++;
+}
+console.log(`Jojo is ${age_count} years old`);
