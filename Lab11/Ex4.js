8db1a6dd59eeaf00d9d30fab007c746284b1f6fd
jlau19 <hl22@hawaii.edu>
2020-10-22 22:32:16 -1000

    xx

--- Lab11/Ex4.js
+++ Lab11/Ex4.js
@@ -1,11 +1,10 @@
-function isNonNegInt(q, returnErrors) {
+function isNonNegInt(val, returnErrors) {
     errors = []; // assume no errors at first
-    if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
-    if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
-    if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-    
-    return returnErrors ? errors : (errors.length == 0);
-    }
+    if(Number(val) != val) errors.push('Not a number!'); // Check if string is a number value
+    if(val < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if(parseInt(val) != val) errors.push('Not an integer!'); // Check that it is an integer
+    return returnErrors ? errors : ((errors.length > 0) ? false : true);
+}
         
         attributes = "Jojo;23;23.5;" + (0.5 - 23);
         pieces = attributes.split(";");
