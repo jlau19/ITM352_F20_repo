80bbab65f87cc4b3afa1440f2e39d1ebf349ce1d
jlau19 <hl22@hawaii.edu>
2020-11-04 23:59:39 -1000

    xx

--- Lau_Jojo_Assignment1/server.js
+++ Lau_Jojo_Assignment1/server.js
@@ -1,51 +1,90 @@
 var express = require('express');
-var app = express();
 var myParser = require("body-parser");
 var fs = require('fs');
-var data = require('./products.json');
-var products = data.products;
-
-app.all('*', function (request, response, next) {
-    console.log(request.method + ' to path ' + request.path);
-    next();
-});
+var products = require("./products.json");
+var app = express();
 
 app.use(myParser.urlencoded({ extended: true }));
 
-app.post("/process_form", function (request, response) {
-    process_quantity_form(request.body, response);
-});
+app.post("/process_invoice", function (request, response, next) {
+    let POST = request.body;
+    if(typeof POST['purchase_submit'] != 'undefined') {
+        console.log('No purchase form data');
+        next();
+    } 
 
-app.use(express.static('./public'));
+    console.log(Date.now() + ': Purchase made from ip ' + request.ip + ' data: ' + JSON.stringify(POST));
 
-app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here
+    var contents = fs.readFileSync('./views/invoice.template', 'utf8');
+    response.send(eval('`' + contents + '`')); // render template string
 
-function isNonNegInt(val, returnErrors = false) {
-    errors = []; // assume no errors at first
-    if (Number(val) != val) {
-        errors.push('Not a number!');// Check if string is a number value
-    } else {
-        if (val < 0) errors.push('Negative value!'); // Check if it is non-negative
-        if (parseInt(val) != val) errors.push('Not an integer!'); // Check that it is an integer
-    }
-    return returnErrors ? errors : ((errors.length > 0) ? false : true);
-}
-
-function process_quantity_form (POST, response) {
-    if (typeof POST['purchase_submit_button'] != 'undefined') {
-        var contents = fs.readFileSync('./views/display_quantity_template.view', 'utf8');
-        receipt = '';
-        for(i in products) {
-            let val = POST[`quantity_textbox${i}`];
-            let product_name = products[i][`name`];
-            let product_price = products[i]['price'];
-            if(isNonNegInt(val)) {
-                receipt += eval('`' + contents + '`');
-            } else {
-                receipt += `<h3><font color="red">${val} is not a valid quantity for ${product_name}!</font></h3>`
+    function display_invoice_table_rows() {
+        subtotal = 0;
+        str = '';
+        for (i = 0; i < products.length; i++) {
+            a_qty = 0;
+            if(typeof POST[`quantity${i}`] != 'undefined') {
+                a_qty = POST[`quantity${i}`];
+            }
+            if (a_qty > 0) {
+                // product row
+                extended_price =a_qty * products[i].price
+                subtotal += extended_price;
+                str += (`
+      <tr>
+        <td align="center" width="43%">${products[i].name}</td>
+        <td align="center" width="11%">${a_qty}</td>
+        <td align="center" width="13%">\$${products[i].price}</td>
+        <td align="center" width="54%">\$${extended_price}</td>
+      </tr>
+      `);
             }
         }
-        response.send(receipt);
-        response.end();
+        // Compute tax
+        tax_rate = 0.0575;
+        tax = tax_rate * subtotal;
+
+        // Compute shipping
+        if (subtotal <= 50) {
+            shipping = 2;
+        }
+        else if (subtotal <= 100) {
+            shipping = 5;
+        }
+        else {
+            shipping = 0.05 * subtotal; // 5% of subtotal
+        }
+
+        // Compute grand total
+        total = subtotal + tax + shipping;
+        
+        return str;
     }
-}
+
+});
+
+app.get("/store", function (request, response) {
+    var contents = fs.readFileSync('./views/display_products.template', 'utf8');
+    response.send(eval('`' + contents + '`')); // render template string
+
+    function display_products() {
+        str = '';
+        for (i = 0; i < products.length; i++) {
+            str += `
+                <section class="item">
+                    <h3>${products[i].name}</h3>
+                    <p><img src="${products[i].image}"></p>
+                    <p>$${products[i].price.toFixed(2)}</p>
+                    <p><label id="quantity${i}_label"}">Quantity:</label></p>
+                    <p><input type="text" placeholder="0" name="quantity${i}"
+                    onkeyup="checkQuantityTextbox(this);"></p>
+                </section>
+            `;
+        }
+        return str;
+    }
+});
+
+app.use(express.static('./static'));
+
+var listener = app.listen(8080, () => { console.log('server started listening on port ' + listener.address().port) });
