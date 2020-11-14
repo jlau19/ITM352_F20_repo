d43a0ba0f492ba91617b235d51505e64adb0b18e
jlau19 <hl22@hawaii.edu>
2020-11-14 13:12:42 -1000

    xx

--- Lab14/EC.js
+++ Lab14/EC.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var myParser = require("body-parser");
 const fs = require('fs');
+const { nextTick } = require('process');
 
 const user_data_filename = 'user_data.json';
 
@@ -12,8 +13,6 @@ if (fs.existsSync(user_data_filename)) {
     var data = fs.readFileSync(user_data_filename, 'utf-8');
     users_reg_data = JSON.parse(data);
 }
-
-users_reg_data = JSON.parse(data);
 
 app.use(myParser.urlencoded({ extended: true }));
 
@@ -43,6 +42,41 @@ app.post("/process_login", function (request, response) {
         }
     } else {
         response.send(`Hey! ${request.body.username} does not exist!`);
+    }
+});
+
+app.get("/register", function (request, response) {
+    // Give a simple register form
+    str = `
+<body>
+<form action="" method="POST">
+<input type="text" name="username" size="40" placeholder="enter username" ><br />
+<input type="password" name="password" size="40" placeholder="enter password"><br />
+<input type="password" name="repeat_password" size="40" placeholder="enter password again"><br />
+<input type="email" name="email" size="40" placeholder="enter email"><br />
+<input type="submit" value="Submit" id="submit">
+</form>
+</body>
+    `;
+    response.send(str);
+});
+
+app.post("/register", function (request, response) {
+    // process a simple register form
+    POST = request.body;
+    if (typeof users_reg_data[request.body.username] == 'undefined' && POST['password'] == POST['repeat_password']) {
+        username = POST['username'];
+        users_reg_data[username] = {};
+        users_reg_data[username].username = username;
+        users_reg_data[username].password = POST['password'];
+        users_reg_data[username].email = POST['email'];
+
+        data = JSON.stringify(users_reg_data);
+        fs.writeFileSync(user_data_filename, data, "utf-8");
+
+        response.redirect('http://localhost:8080/login');
+    } else {
+        response.redirect('http://localhost:8080/register');
     }
 });
 
--- Lab14/Ex4.js
+++ Lab14/Ex4.js
@@ -13,8 +13,6 @@ if (fs.existsSync(user_data_filename)) {
     users_reg_data = JSON.parse(data);
 }
 
-users_reg_data = JSON.parse(data);
-
 app.use(myParser.urlencoded({ extended: true }));
 
 app.get("/login", function (request, response) {
@@ -43,6 +41,41 @@ app.post("/process_login", function (request, response) {
         }
     } else {
         response.send(`Hey! ${request.body.username} does not exist!`);
+    }
+});
+
+app.get("/register", function (request, response) {
+    // Give a simple register form
+    str = `
+<body>
+<form action="" method="POST">
+<input type="text" name="username" size="40" placeholder="enter username" ><br />
+<input type="password" name="password" size="40" placeholder="enter password"><br />
+<input type="password" name="repeat_password" size="40" placeholder="enter password again"><br />
+<input type="email" name="email" size="40" placeholder="enter email"><br />
+<input type="submit" value="Submit" id="submit">
+</form>
+</body>
+    `;
+    response.send(str);
+});
+
+app.post("/register", function (request, response) {
+    // process a simple register form
+    POST = request.body;
+    if (typeof users_reg_data[request.body.username] == 'undefined' && POST['password'] == POST['repeat_password']) {
+        username = POST['username'];
+        users_reg_data[username] = {};
+        users_reg_data[username].username = username;
+        users_reg_data[username].password = POST['password'];
+        users_reg_data[username].email = POST['email'];
+
+        data = JSON.stringify(users_reg_data);
+        fs.writeFileSync(user_data_filename, data, "utf-8");
+
+        response.redirect('http://localhost:8080/login');
+    } else {
+        response.redirect('http://localhost:8080/register');
     }
 });
 
