4e2106bce36435687e142d2aad5e38fcfc303e17
jlau19 <hl22@hawaii.edu>
2020-12-17 17:18:13 -1000

    getting there let's gooo

--- Lau_Jojo_Assignment3/server.js
+++ Lau_Jojo_Assignment3/server.js
@@ -12,6 +12,7 @@ var fs = require('fs');
 var app = express();
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var nodemailer = require('nodemailer');
 
 // Variables to use later
 var quantity_data;
@@ -21,13 +22,13 @@ const user_data_filename = 'user_data.json';
 // Parse body of requests with application/x-www-form-urlencoded content type
 app.use(myParser.urlencoded({ extended: true }));
 
-app.post("/get_products", function (request, response) {
-    response.json(products_data);
-});
-
 // Lab 15 Ex 1 & 2, to use cookies and session
 app.use(cookieParser());
 app.use(session({ secret: "ITM352 rocks!" }));
+
+app.post("/get_products", function (request, response) {
+    response.json(products_data);
+});
 
 // lab 14 Ex 2
 // check if file exists before reading
@@ -35,7 +36,7 @@ if (fs.existsSync(user_data_filename)) {
     stats = fs.statSync(user_data_filename);
 
     var data = fs.readFileSync(user_data_filename, 'utf-8');
-    users_reg_data = JSON.parse(data);
+    var users_reg_data = JSON.parse(data);
 }
 
 // Assignment 3 code Example 2 for server.js 
@@ -48,6 +49,7 @@ app.all('*', function (request, response, next) {
     next();
 });
 
+//
 app.get("/add_to_cart", function (request, response, next) {
     var products_key = request.query['products_key']; // get the product key sent from the form post
     var quantities = request.query['quantities'].map(Number); // Get quantities from the form post and convert strings from form post to numbers
@@ -92,6 +94,30 @@ app.get("/cart", function (request, response) {
         cart_str = `Hi ${user_name}, please review your shopping cart!`
     }
 
+    /*function add_items() {
+        // For each product key in session cart array
+        for (pk in request.session.cart) {
+
+            // For each specific item in the product key of session cart array
+            for (i = 0; i < request.session.cart[pk].length; i++) {
+                qty = request.session.cart[pk][i];
+                qty += 1;
+            }
+        }
+    }
+
+    function remove_items() {
+        // For each product key in session cart array
+        for (pk in request.session.cart) {
+
+            // For each specific item in the product key of session cart array
+            for (i = 0; i < request.session.cart[pk].length; i++) {
+                qty = request.session.cart[pk][i];
+                qty -= 1;
+            }
+        }
+    }*/
+
     subtotal = 0;
     invoice_rows = '';
 
@@ -100,13 +126,9 @@ app.get("/cart", function (request, response) {
 
         // For each specific item in the product key of session cart array
         for (i = 0; i < request.session.cart[pk].length; i++) {
+            qty = request.session.cart[pk][i];
 
-            qty = 0;
-            val = request.session.cart[pk][i];
-
-            if (val > 0) {
-                qty = val;
-
+            if (qty > 0) {
                 // product row
                 extended_price = qty * products_data[pk][i].price
                 subtotal += extended_price;
@@ -149,21 +171,6 @@ app.get("/get_cart", function (request, response) {
     response.json(request.session.cart);
 });
 
-// Lab 13 Ex 3
-// Response when /purchase is requested at shopping cart page, loads invoice
-app.get("/purchase", function (request, response) {
-    // add invoice stuff here
-    
-    // If is_valid stays true after all validations, redirect to login
-    if (is_valid == true) {
-        quantity_data = POST;
-        response.redirect('./login');
-    } else {
-        // If is_valid is false, redirect to error page
-        var err_contents = fs.readFileSync('./views/errors/qtyerror.template', 'utf8');
-        response.send(eval('`' + err_contents + '`')); // render template string
-    }
-});
 
 // Lab 14 Ex 3
 // Response when /login is requested
@@ -201,9 +208,10 @@ app.post("/process_login", function (request, response) {
             var now = new Date();
             request.session.lastLogin = now.getTime();
             console.log(`${request.body.username} logged in on ${request.session.lastLogin}`);
-            response.cookie('username', request.body.username, { maxAge: 300 * 1000 });
+            // Set cookie to expire after 15 minutes
+            response.cookie('username', request.body.username, { maxAge: 900 * 1000 });
 
-            response.redirect('./products.html?products_key=jewelry');
+            response.redirect('./loginsuccess.html');
 
         } else {
             // If passwords do not match, redirect to error page
@@ -214,6 +222,93 @@ app.post("/process_login", function (request, response) {
         // If username does not exist, redirect to error page
         var err_contents = fs.readFileSync('./views/errors/usererror.template', 'utf8');
         response.send(eval('`' + err_contents + '`')); // render template string
+    }
+});
+
+// Modified from Assignment 3 code examples: Example 3 creating invoice and email user
+// Response when /checkout is requested at shopping cart page, loads invoice
+app.get("/checkout", function (request, response) {
+    if (typeof request.cookies.username != 'undefined') {
+        user_name = request.cookies.username;
+
+        subtotal = 0;
+        invoice_rows = '';
+
+        // For each product key in session cart array
+        for (pk in request.session.cart) {
+
+            // For each specific item in the product key of session cart array
+            for (i = 0; i < request.session.cart[pk].length; i++) {
+                qty = request.session.cart[pk][i];
+
+                if (qty > 0) {
+                    // product row
+                    extended_price = qty * products_data[pk][i].price
+                    subtotal += extended_price;
+                    invoice_rows += (`
+  <tr>
+    <td align="center" width="43%">${products_data[pk][i]['name']}</td>
+    <td align="center" width="11%">${qty}</td>
+    <td align="center" width="13%">\$${products_data[pk][i]['price']}</td>
+    <td align="center" width="54%">\$${extended_price}</td>
+  </tr>
+  `);
+                }
+            }
+        }
+        // Compute tax
+        tax_rate = 0.0575;
+        tax = tax_rate * subtotal;
+
+        // Compute shipping
+        if (subtotal <= 50) {
+            shipping = 2;
+        }
+        else if (subtotal <= 100) {
+            shipping = 5;
+        }
+        else {
+            shipping = 0.05 * subtotal; // 5% of subtotal
+        }
+
+        // Compute grand total
+        total = subtotal + tax + shipping;
+
+        // Set up mail server. Only will work on UH Network due to security restrictions
+        var transporter = nodemailer.createTransport({
+            service: "mail.hawaii.edu",
+            port: 25,
+            secure: false, // use TLS
+            tls: {
+                // do not fail on invalid certs
+                rejectUnauthorized: false
+            }
+        });
+
+        var user_email = users_reg_data[user_name].email;
+        // Sends invoice to user's email from my unimportant email address
+        var mailOptions = {
+            from: 'jlaaau@gmail.com',
+            to: user_email,
+            subject: 'Your Invoice from Cherry On Top',
+            html: invoice_rows
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+                email_str = `There was an error and your invoice could not be emailed to ${user_email} :(`;
+            } else {
+                email_str = `Your invoice was mailed to ${user_email}! :)`;
+            }
+
+        // Directs to checkout page
+        var contents = fs.readFileSync('./views/checkout.template', 'utf8');
+        response.send(eval('`' + contents + '`')); // render template string
+
+        });
+    } else {
+        response.redirect('./login');
     }
 });
 
@@ -279,7 +374,7 @@ app.post("/process_register", function (request, response) {
         reg_info_str = JSON.stringify(users_reg_data);
         fs.writeFileSync(user_data_filename, reg_info_str);
         // rediret to login page
-        response.redirect('./loginsuccess.html');
+        response.redirect('./registersuccess.html');
     } else {
         // Displays all errors in error page if there are any
         errs = err.join('! ');
